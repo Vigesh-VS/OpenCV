ae5e5c6c6dd3f5ce84693a359a2b4daaae07e903
Vigesh-VS <thevigesh17@gmail.com>
2025-05-28 13:39:28 +0530

    Video resizing cod uploaded

---
+++ 1. Basics/Resizing_video.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+capture = cv.VideoCapture('Videos/Dog.mp4')
+
+if not capture.isOpened():
+    print("Error opening video file")
+    exit()
+
+while True:
+    isTrue, Frame = capture.read()
+    Resized_video = cv.resize(Frame, (500,500))
+
+    cv.imshow('Original video' , Frame)
+    cv.imshow('Resized video' , Resized_video)
+
+    if cv.waitKey(10) & 0xff == ord('f'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
